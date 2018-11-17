79deab0c2067e5b77c0c4119850eae8b269eccbd
kanoflordelis <ka958376@gmaio>
2018-11-17 12:12:25 -0800

    Implement DLPriority Queue
    Im not sure if it's correct or not...

--- src/DLPriorityQueue.java
+++ src/DLPriorityQueue.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DLPriorityQueue<K extends Comparable, V> implements VCPriorityQueue<K, V>{
@@ -6,7 +6,7 @@ public class DLPriorityQueue<K extends Comparable, V> implements VCPriorityQueue
     private List<Entry<K, V>> queue2;
 
     public DLPriorityQueue() {
-        queue2 = new ArrayList<>();
+        queue2 = new LinkedList<>();
     }
 
     @Override
@@ -21,22 +21,49 @@ public class DLPriorityQueue<K extends Comparable, V> implements VCPriorityQueue
 
     @Override
     public Entry<K, V> enqueue(K key, V value) throws IllegalArgumentException {
+        Entry<K, V> before = new Entry<>(key, value);
+        Entry<K, V> after = new Entry<>(key, value);
         Entry<K, V> newEntry = new Entry<>(key, value);
-        return null;
+        queue2.add(before);
+        queue2.add(after);
+        queue2.add(newEntry);
+        if (before == null && after == null) {
+            before = newEntry;
+            after = newEntry;
+        } else {
+            if (newEntry.getKey().compareTo(before.getKey()) > 0) {
+                before = newEntry;
+            } else if (newEntry.getKey().compareTo(before.getKey()) < 0) {
+                after = newEntry;
+            }
+        }
+        return newEntry;
     }
 
     @Override
     public Entry<K, V> peek() {
-        return null;
+        return queue2.get(0);
     }
 
     @Override
     public Entry<K, V> dequeueMin() {
-        return null;
+        return queue2.remove(0);
+    }
+
+    public List<Entry<K, V>> getQueue2() {
+        return queue2;
     }
 
     @Override
     public VCPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
-        return null;
+        DLPriorityQueue<K, V> otherQ = ((DLPriorityQueue) other);
+        for (Entry<K, V> entry : otherQ.getQueue2()) {
+            for (int i = 0; i < queue2.size(); i++) {
+                if (queue2.get(i).getKey().compareTo(entry.getKey()) < 0) {
+                    entry = queue2.get(i);
+                }
+            }
+        }
+        return this;
     }
 }
