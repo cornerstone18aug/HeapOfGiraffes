2836c8bc5a31389180e3c31442e5aa9bafc11b22
kanoflordelis <ka958376@gmaio>
2018-11-19 14:16:00 -0800

    Add BHPriorityQueue

--- src/BHPriorityQueue.java
+++ src/BHPriorityQueue.java
@@ -1,2 +1,37 @@
-public class BHPriorityQueue {
+import java.util.LinkedList;
+import java.util.List;
+
+public class BHPriorityQueue <K extends Comparable, V> implements VCPriorityQueue<K, V> {
+
+    private LinkedList<Entry<K, V>> queue;
+
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public Entry<K, V> enqueue(K key, V value) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> peek() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> dequeueMin() {
+        return null;
+    }
+
+    @Override
+    public VCPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
+        return null;
+    }
 }
